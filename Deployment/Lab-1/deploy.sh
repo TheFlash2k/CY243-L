286eca336595a2485a0b62407ff47bd43d749771
Ali Taqi <alitaqi2000@gmail.com>
2023-09-16 06:54:10 +0500

    Updated deployment scripts

--- Deployment/Lab-1/deploy.sh
+++ Deployment/Lab-1/deploy.sh
@@ -2,25 +2,25 @@
 
 ## Author: @TheFlash2k
 
-echo "[+] Checking if docker is installed"
+echo -e  "[\e[34m*\e[0m] Checking if docker is installed"
 if ! command -v docker &> /dev/null; then
-    echo "[!] Docker is not found on your system. Please install using 'sudo apt install docker.io docker-compose'"
+    echo -e "[\e[31m!\e[0m] Docker is not found on your system. Please install using 'sudo apt install docker.io docker-compose'"
     exit 1
 fi
 
-echo -n "[*] Found "
+echo -e -n "[\e[32m+\e[0m] Found "
 docker -v
 
-echo "[+] Checking if user is part of docker group"
+echo -e  "[\e[34m*\e[0m] Checking if user is part of docker group"
 
-getent group docker | grep $(whoami) 2>/dev/null
+getent group docker | grep $(whoami) &>/dev/null
 
-if [[ $? != 0 ]]; then
-    echo "[*] User $(whoami) not part of docker group! Please add the user using 'usermod -aG docker $(whoami)' command and re-login."
+if [ $? != 0 ]; then
+    echo -e  "[\e[31m!\e[0m] User $(whoami) not part of docker group! Please add the user using 'usermod -aG docker $(whoami)' command and re-login."
     exit 1
 fi
 
-echo "[*] Preparing the lab..."
+echo -e  "[\e[34m*\e[0m] Preparing the lab..."
 docker build -t cy243l-lab1 ./data &>/dev/null
-echo "[+] Done!. Spawning an interactive shell into the lab environment"
-docker run -it --rm --name "cy243l-lab1-instance" --hostname 'lab1' cy243l-lab1
+echo -e  "[\e[32m+\e[0m] Done!. Spawning an interactive shell into the lab environment"
+docker run -it --rm --name "cy243l-lab1-instance" --hostname "lab1" "cy243l-lab1"
