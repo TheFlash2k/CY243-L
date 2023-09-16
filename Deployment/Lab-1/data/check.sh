4dfde6568799f2ffd10d7ca206ca73d48239ce6e
Ali Taqi <alitaqi2000@gmail.com>
2023-09-16 07:11:19 +0500

    Fixed comparison of roll numbers

--- Deployment/Lab-1/data/check.sh
+++ Deployment/Lab-1/data/check.sh
@@ -32,30 +32,30 @@ shopt -u nocaseglob
 if [ $_res_dir != 0 ] || [ $_res_dir_git != 0 ]; then failed; else passed; fi
 echo "CY243-L Repository cloned locally"
 
-if [ -d /tmp/test-21* ] && [ -f /tmp/test-21*/test2.txt ]; then
-    (cat /tmp/test-21*/test2.txt | grep 'This file is created by') &>/dev/null
+if [ -d /tmp/test-2* ] && [ -f /tmp/test-2*/test2.txt ]; then
+    (cat /tmp/test-2*/test2.txt | grep 'This file is created by') &>/dev/null
     if [[ $? != 0 ]]; then failed; else passed; fi
 else
     failed
 fi
 echo "Checking /tmp/test-<roll-number> and test2.txt inside the folder and checking the file contents."
 
-folder_owner=`stat -c %U /tmp/test-21* 2>/dev/null`
-file_group=`stat -c %G /tmp/test-21*/test2.txt 2>/dev/null`
+folder_owner=`stat -c %U /tmp/test-2* 2>/dev/null`
+file_group=`stat -c %G /tmp/test-2*/test2.txt 2>/dev/null`
 
 if [[ $folder_owner != $username ]]; then failed; else passed; fi
 echo -n "Checking if $username is owner of /tmp/test-<roll-number>"
-if [ -d /tmp/test-21* ]; then echo " [Actual owner: $folder_owner]"; else echo; fi
+if [ -d /tmp/test-2* ]; then echo " [Actual owner: $folder_owner]"; else echo; fi
 
 if [[ $file_group != $grpname ]]; then failed; else passed; fi
 echo -n "Checking if $grpname is group-owner of /tmp/test-<roll-number>/test2.txt"
-if [ -f /tmp/test-21*/test2.txt ]; then echo " [Actual group: $file_group]"; else echo; fi
+if [ -f /tmp/test-2*/test2.txt ]; then echo " [Actual group: $file_group]"; else echo; fi
 
-perms=`stat -c %A /tmp/test-21*/test2.txt 2>/dev/null`
+perms=`stat -c %A /tmp/test-2*/test2.txt 2>/dev/null`
 
 if [[ $perms != "-rwxr-x---" ]]; then failed; else passed; fi
 echo -n "Checking the permissions on the file (if they match the criteria defined in the task.)"
-if [ -f /tmp/test-21*/test2.txt ]; then echo " [Actual Permissions: $perms]"; else echo; fi
+if [ -f /tmp/test-2*/test2.txt ]; then echo " [Actual Permissions: $perms]"; else echo; fi
 
 if [[ tests -ne t_tests ]]; then
     exit 1
